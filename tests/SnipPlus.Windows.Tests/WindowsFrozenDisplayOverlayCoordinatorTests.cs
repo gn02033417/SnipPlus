23e4a1ad8fe4d032405f46c07fe8cd7e35360b68
gn02033417 <gn02033417@gmail.com>
2026-07-28 17:13:33 +0800

    test: assert system crosshair overlay contract

---
+++ tests/SnipPlus.Windows.Tests/WindowsFrozenDisplayOverlayCoordinatorTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using Microsoft.UI.Input;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SnipPlus.Windows;
+
+namespace SnipPlus.Windows.Tests;
+
+[TestClass]
+public sealed class WindowsFrozenDisplayOverlayCoordinatorTests
+{
+    [TestMethod]
+    [TestCategory("Unit")]
+    [TestCategory("Contract")]
+    public void OverlayUsesSystemCursorSurfaceWithoutDrawnCrosshairLines()
+    {
+        var overlaySurface = typeof(WindowsFrozenDisplayOverlayCoordinator)
+            .GetNestedType("OverlaySurface", BindingFlags.NonPublic);
+
+        Assert.IsNotNull(overlaySurface);
+        Assert.IsNull(overlaySurface.GetField(
+            "_crosshairHorizontal",
+            BindingFlags.Instance | BindingFlags.NonPublic));
+        Assert.IsNull(overlaySurface.GetField(
+            "_crosshairVertical",
+            BindingFlags.Instance | BindingFlags.NonPublic));
+
+        var canvasField = overlaySurface.GetField(
+            "_canvas",
+            BindingFlags.Instance | BindingFlags.NonPublic);
+        Assert.IsNotNull(canvasField);
+        Assert.AreEqual("CrosshairCanvas", canvasField.FieldType.Name);
+
+        var cursorProperty = canvasField.FieldType.GetProperty(
+            "Cursor",
+            BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+        Assert.IsNotNull(cursorProperty);
+        Assert.AreEqual(typeof(InputCursor), cursorProperty.PropertyType);
+    }
+}
